7c4765c9582e9a5317b067bce0eff29baa0cf1f3
Noor Asmar <noor_jb@outlook.com>
2023-04-05 11:02:42 +0300

    create flip card

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,5 +2,7 @@ document.getElementById("start-btn").addEventListener("click", RemoveLayer);
 
 function RemoveLayer(){
     let overlay = document.getElementsByClassName('overlay');
+    let username = document.getElementById("username").value;
+    document.querySelector("header span").innerHTML = username;
     overlay[0].remove();
 }
