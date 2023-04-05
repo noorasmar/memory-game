dacd2a32c58db414a3e49629d4b9cbf4b5543553
Noor Asmar <noor_jb@outlook.com>
2023-04-05 15:31:10 +0300

    complete the functionality

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,10 +1,15 @@
 // document.getElementById("start-btn").addEventListener("click", removeLayer);
-const flipCards = document.querySelectorAll(".flip-card");
+const flipCards = document.querySelectorAll(".flip-card:not(.close)");
+let arr = []
+const cards = []
+let count = 0
+
 flipCards.forEach(function(flipCard) {
     flipCard.addEventListener("click", function() {
         const flipCardInner = flipCard.querySelector(".flip-card-inner");
-        flipCardInner.classList.add("flipped");
-        alert(flipCard.dataset.card);
+        flipCardInner.classList.add("flipped")
+        cards.push(flipCardInner)
+        setData(flipCard.dataset.card)
     });
 });
 
@@ -13,4 +18,29 @@ function removeLayer(){
     let username = document.getElementById("username").value;
     document.querySelector("header span").innerHTML = username;
     overlay[0].remove();
+}
+
+function setData(data){
+    if(arr.length < 2)
+    {
+        arr.push(data)
+    }
+    if(arr.length === 2){
+        const result = checkData(arr[0], arr[1])
+        if(result){
+        }else{
+            setTimeout(() => {
+                cards[cards.length-1].classList.remove('flipped')
+                cards[cards.length-2].classList.remove('flipped')
+            }, 1000);
+            count++
+            document.getElementById('count').innerText = count
+        }
+        arr.pop()
+        arr.pop()
+    }
+}
+
+function checkData(data1, data2){
+    return data1 === data2
 }
