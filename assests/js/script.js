9f08cff9b2013f804ee6353b5c35c50675c1633b
Noor Asmar <noor_jb@outlook.com>
2023-04-05 11:47:02 +0300

    buil the board

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,14 @@
-document.getElementById("start-btn").addEventListener("click", RemoveLayer);
+// document.getElementById("start-btn").addEventListener("click", removeLayer);
+const flipCards = document.querySelectorAll(".flip-card");
+flipCards.forEach(function(flipCard) {
+    flipCard.addEventListener("click", function() {
+        const flipCardInner = flipCard.querySelector(".flip-card-inner");
+        flipCardInner.classList.add("flipped");
+        alert(flipCard.dataset.card);
+    });
+});
 
-function RemoveLayer(){
+function removeLayer(){
     let overlay = document.getElementsByClassName('overlay');
     let username = document.getElementById("username").value;
     document.querySelector("header span").innerHTML = username;
